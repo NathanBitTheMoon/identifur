39e034482ca8ff4891febc4b9c4ad18c1beff284
NathanBitTheMoon <fuzzy.hazza.gld@protonmail.com>
2021-02-23 21:43:31 +1100

    Added scripts for creating tables

---
+++ db/create-tables.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE `identifur`;
+USE `identifur`;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE `faLinks` (
+    id INT NOT NULL PRIMARY KEY,
+    link TEXT NOT NULL,
+    thumb TEXT NOT NULL,
+    title TEXT NOT NULL
+    resourceId INT NOT NULL AUTO_INCREMENT
+);
+
+CREATE TABLE `resources` (
+    id INT NOT NULL PRIMARY KEY,
+    resourceDir TEXT,
+    fursona INT
+);
+
+CREATE TABLE `suits` (
+    id INT NOT NULL,
+    fursonaName TEXT,
+    fursonaSpecies TEXT,
+    fursonaGender TEXT,
+    ownerSocial TEXT
+);
+
+COMMIT;
