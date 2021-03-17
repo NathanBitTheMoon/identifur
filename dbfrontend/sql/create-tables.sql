842b9e3f6c888c61b41130cb82cd96d66fd4a4fa
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-03-18 09:09:27 +1100

    Changed SQL password

--- dbfrontend/sql/create-tables.sql
+++ dbfrontend/sql/create-tables.sql
@@ -27,12 +27,12 @@ CREATE TABLE `sqlCreds` (
 );
 
 -- It is recommended that you change the root user password!!!!
-CREATE USER 'identifur-fr-root'@'localhost' IDENTIFIED BY 'identifur-root-user';
+CREATE USER 'identifur-fr-root'@'localhost' IDENTIFIED BY '_aE#t9T_bS%Fa|Rd*^I+5">v44bBDfyc7q;mFR.+?].Bw[sN^0';
 GRANT UPDATE, SELECT, DELETE ON identifur.* TO 'identifur-fr-root'@'localhost';
 FLUSH PRIVILEGES;
 
 INSERT INTO `sqlCreds` (id, uname, host, passwd)
-    VALUES (0, 'identifur-fr-root', 'localhost', 'identifur-root-user');
+    VALUES (0, 'identifur-fr-root', 'localhost', '_aE#t9T_bS%Fa|Rd*^I+5">v44bBDfyc7q;mFR.+?].Bw[sN^0');
 
 INSERT INTO `users` (uname, passwd, mysqlCreds, permModUser, promptPasswdChange)
     VALUES ('root', '4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2', 0, 1, 1);
