0e85d393f8d66a8b83f5b812199dabf14b4d552c
NathanBitTheMoon <fuzzy.hazza.gld@protonmail.com>
2021-03-18 08:30:22 +1100

    Added setup scripts and apache config for database frontend

---
+++ dbfrontend/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mv ./apache/identifur-frontend.conf /etc/apache2/sites-available/identifur-frontend.conf
+a2ensite identifur-frontend.conf
+a2dissite 000-default.conf
+
+if [ -d "/var/www/identifur-frontend" ]
+then
+    mkdir /var/www/identifur-frontend/
+
+mv -r html/* /var/www/identifur-frontend/
+
+systemctl reload apache2
