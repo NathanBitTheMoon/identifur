065c26c02189d7162fd85243a206227cd4001cfd
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-03-18 14:15:35 +1100

    lol why would it

--- dbfrontend/html/index.php
+++ dbfrontend/html/index.php
@@ -3,13 +3,19 @@
     include './php/auth_methods.php';
     session_start();
 
+    error_reporting(E_ALL);
+    ini_set('display_errors', TRUE);
+    ini_set('display_startup_errors', TRUE);
+
     if (isset($_GET["error"])) {
         include './html/login.error.html';
     } else if (isset($_GET["success"]) && isset($_SESSION["auth"])) {
         echo "Yes";
+
         if (AuthMethods::check_prompt($_SESSION["uid"])) {
             // Redirect the user to a password change page
             header("Location: update_password.php?reason=policy");
+            echo "Yes";
         }
     }
 ?>
