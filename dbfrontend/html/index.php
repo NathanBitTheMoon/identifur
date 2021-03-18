8cba4da65b9d6468101e0cdbcc2b5f3bad8816cb
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-03-18 14:12:13 +1100

    lol why would it

--- dbfrontend/html/index.php
+++ dbfrontend/html/index.php
@@ -5,6 +5,7 @@
     if (isset($_GET["error"])) {
         include './html/login.error.html';
     } else if (isset($_GET["success"]) && isset($_SESSION["auth"])) {
+        echo "Yes";
         if (AuthMethods::check_prompt($_SESSION["uid"])) {
             // Redirect the user to a password change page
             header("Location: update_password.php?reason=policy");
