749b9d5065d752cc82cf5854f055a3d3b2056a79
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-03-18 14:41:01 +1100

    ...

--- dbfrontend/html/auth.php
+++ dbfrontend/html/auth.php
@@ -13,7 +13,7 @@
 
         include 'php/auth_methods.php';
         $user = AuthMethods::authenticate_user($uid, $pwd);
-        echo $user; 
+        echo json_encode($user); 
 
         if ($user == null) {
             // Authentication failed
